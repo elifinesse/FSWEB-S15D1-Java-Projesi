657c1f3cb5ee8dc0f22c238f2717e2d0c55b2794
elifinesse <133097200+elifinesse@users.noreply.github.com>
2023-10-02 16:50:09 +0300

    MobilePhone done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        MobilePhone mp = new MobilePhone("123456");
+        Contact c1 = new Contact("elif", "654321");
+        Contact c2 = new Contact("person", "987654");
+        Contact c3 = new Contact("kisi", "987432");
+        Contact c4 = new Contact("another", "000");
+
+        mp.addNewContact(c1);
+        mp.addNewContact(c3);
+        mp.addNewContact(c4);
+        mp.updateContact(c1, c2);
+        System.out.println(mp.findContact("kisi"));
+        System.out.println(mp.findContact(c1));
+        mp.removeContact(c4);
+        mp.printContact();
+
+
+        System.out.println(mp.getMyContacts());
     }
 }
